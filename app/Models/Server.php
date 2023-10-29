eb10ec5c17ff558c80a8e84b9bb46d4f34618b05
dannypas00 <github@dannypas.nl>
2023-10-29 21:35:42 +0100

    Install package, update model and migration

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use App\Enums\ServerStatus;
 use App\Rcon\Rcon;
 use Crypt;
+use Database\Factories\ServerFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\Server
@@ -23,35 +28,49 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  * @property string|null $local_ip
  * @property string $public_ip
  * @property string $rcon_password
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  * @property int $current_players
  * @property int $maximum_players
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $users
+ * @property-read Collection<int, User> $users
  * @property-read int|null $users_count
- * @method static \Database\Factories\ServerFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Server newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Server newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Server query()
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereCurrentPlayers($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereEnabled($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereIcon($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereLocalIp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereMaximumPlayers($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server wherePort($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server wherePublicIp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereRconPassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereRconPort($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereUpdatedAt($value)
+ * @method static ServerFactory factory($count = null, $state = [])
+ * @method static Builder|Server newModelQuery()
+ * @method static Builder|Server newQuery()
+ * @method static Builder|Server query()
+ * @method static Builder|Server whereCreatedAt($value)
+ * @method static Builder|Server whereCurrentPlayers($value)
+ * @method static Builder|Server whereDescription($value)
+ * @method static Builder|Server whereEnabled($value)
+ * @method static Builder|Server whereIcon($value)
+ * @method static Builder|Server whereId($value)
+ * @method static Builder|Server whereLocalIp($value)
+ * @method static Builder|Server whereMaximumPlayers($value)
+ * @method static Builder|Server whereName($value)
+ * @method static Builder|Server wherePort($value)
+ * @method static Builder|Server wherePublicIp($value)
+ * @method static Builder|Server whereRconPassword($value)
+ * @method static Builder|Server whereRconPort($value)
+ * @method static Builder|Server whereUpdatedAt($value)
  * @property string|null $player_list Comma-separated list of users
- * @method static \Illuminate\Database\Eloquent\Builder|Server wherePlayerList($value)
+ * @method static Builder|Server wherePlayerList($value)
  * @property ServerStatus $status
- * @method static \Illuminate\Database\Eloquent\Builder|Server whereStatus($value)
- * @mixin \Eloquent
+ * @method static Builder|Server whereStatus($value)
+ * @property int $enable_ftp
+ * @property int $is_sftp
+ * @property int $use_ssh_auth
+ * @property int|null $ftp_port
+ * @property string|null $ftp_host
+ * @property string|null $ftp_username Contains private key when using ssh key auth
+ * @property string|null $ftp_password Contains pass phrase when using ssh key auth
+ * @method static Builder|Server whereEnableFtp($value)
+ * @method static Builder|Server whereFtpHost($value)
+ * @method static Builder|Server whereFtpPassword($value)
+ * @method static Builder|Server whereFtpPort($value)
+ * @method static Builder|Server whereFtpUsername($value)
+ * @method static Builder|Server whereIsSftp($value)
+ * @method static Builder|Server whereUseSshAuth($value)
+ * @mixin Eloquent
  */
 class Server extends Model
 {
@@ -63,16 +82,26 @@ class Server extends Model
         'name',
         'description',
         'icon',
+        'enabled',
+        // Connection info
         'local_ip',
         'public_ip',
         'port',
         'rcon_port',
-        'enabled',
         'rcon_password',
+        // Cached info
         'current_players',
         'maximum_players',
         'player_list',
         'status',
+        // FTP settings
+        'enable_ftp',
+        'is_sftp',
+        'use_ssh_auth',
+        'ftp_port',
+        'ftp_host',
+        'ftp_username',
+        'ftp_password',
     ];
 
     protected $casts = [
@@ -80,6 +109,22 @@ class Server extends Model
     ];
 
     public function rconPassword(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (string $value): string => Crypt::decrypt($value),
+            set: static fn (string $value): string => Crypt::encrypt($value),
+        );
+    }
+
+    public function ftpUsername(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (string $value): string => Crypt::decrypt($value),
+            set: static fn (string $value): string => Crypt::encrypt($value),
+        );
+    }
+
+    public function ftpPassword(): Attribute
     {
         return Attribute::make(
             get: static fn (string $value): string => Crypt::decrypt($value),
