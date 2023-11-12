dc8e920274038387fcbac4daa61784b4022f570c
dannypas00 <github@dannypas.nl>
2023-11-12 15:25:55 +0100

    Cleaned up controller

--- app/Http/Controllers/Servers/ServerLogStreamController.php
+++ app/Http/Controllers/Servers/ServerLogStreamController.php
@@ -14,54 +14,26 @@ class ServerLogStreamController extends Controller
 
     public function __invoke(int $serverId): StreamedResponse
     {
-        // https://pineco.de/simple-event-streaming-in-laravel/
-        return response()->stream(function () use ($serverId) {
-            $ftp = $this->getFtp($serverId);
+        return response()
+            ->stream(function () use ($serverId) {
+                $ftp = $this->getFtp($serverId);
 
-            $lastSize = 0;
+                $lastSize = 0;
 
-            while (true) {
-                if (connection_aborted() === 1) {
-                    break;
+                while (sleep(0.1) === 0) {
+                    if (connection_aborted() === 1) {
+                        break;
+                    }
+
+                    $this->tryReadFile($ftp, $lastSize);
                 }
-
-                $currentSize = $ftp->size(self::LOG_NAME);
-
-                if ($currentSize > $lastSize) {
-                    Log::debug('File size increased', ['lastSize' => $lastSize, 'currentSize' => $currentSize]);
-
-                    $stream = $ftp->readStream(self::LOG_NAME);
-
-                    $message = base64_encode(stream_get_contents($stream, null, $lastSize));
-                    $lastSize = $currentSize;
-
-                    // Send data
-                    echo "event: ping" . PHP_EOL, "data: $message", PHP_EOL . PHP_EOL;
-
-                    // Push and end stream
-                    flush();
-                    clearstatcache();
-                    fclose($stream);
-                } else {
-                    Log::debug('No new info');
-                }
-
-                sleep(0.1);
-            }
-        },
-            200,
-            [
+            }, 200, [
                 'X-Accel-Buffering' => 'no',
                 'Cache-Control'     => 'no-cache',
-                'Content-Type'      => 'text/event-stream',
-            ]
-        );
+                'Content-Type'      => 'text/event-stream'
+            ]);
     }
 
-    /**
-     * @param int $serverId
-     * @return FilesystemAdapter
-     */
     public function getFtp(int $serverId): FilesystemAdapter
     {
         return Auth::user()
@@ -69,5 +41,29 @@ class ServerLogStreamController extends Controller
             ->where('servers.id', $serverId)
             ->first()
             ?->ftp();
+    }
+
+    private function tryReadFile(FilesystemAdapter $ftp, int &$lastSize): void
+    {
+        $currentSize = $ftp->size(self::LOG_NAME);
+
+        if ($currentSize > $lastSize) {
+            Log::debug('File size increased', ['lastSize' => $lastSize, 'currentSize' => $currentSize]);
+
+            $stream = $ftp->readStream(self::LOG_NAME);
+
+            $message = base64_encode(stream_get_contents($stream, null, $lastSize));
+            $lastSize = $currentSize;
+
+            // Send data
+            echo "event: ping" . PHP_EOL, "data: $message", PHP_EOL . PHP_EOL;
+
+            // Push and end stream
+            flush();
+            clearstatcache();
+            fclose($stream);
+        } else {
+            Log::debug('No new info');
+        }
     }
 }
