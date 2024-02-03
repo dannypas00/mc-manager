df24e3a2de566598068689b8ee524ac0fd106259
dannypas00 <github@dannypas.nl>
2024-02-03 21:53:31 +0100

    FormTextArea component

--- resources/js/Components/Form/FormInput.vue
+++ resources/js/Components/Form/FormInput.vue
@@ -8,20 +8,18 @@
       Website
     </label>
 
-    <div>
-      <div class="flex rounded-md shadow-sm ring-1 ring-inset pl-2 ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-        <span v-if="$slots.prefix" class="flex select-none items-center pl-1 text-gray-500 sm:text-sm">
-          <slot name="prefix"/>
-        </span>
+    <div class="flex rounded-md shadow-sm ring-1 ring-inset pl-2 ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+      <span v-if="$slots.prefix" class="flex select-none items-center pl-1 text-gray-500 sm:text-sm">
+        <slot name="prefix"/>
+      </span>
 
-        <input
-          :id="id"
-          :placeholder="placeholder"
-          type="text"
-          name="website"
-          class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-        >
-      </div>
+      <input
+        :id="id"
+        :placeholder="placeholder"
+        :name="label"
+        type="text"
+        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+      >
     </div>
   </div>
 </template>
@@ -33,15 +31,15 @@ defineProps({
     required: true,
   },
 
+  id: {
+    type: String,
+    required: true,
+  },
+
   label: {
     type: String,
     required: false,
     default: undefined,
-  },
-
-  id: {
-    type: String,
-    required: true,
   },
 
   placeholder: {
---
+++ resources/js/Components/Form/FormTextArea.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="col-span-full">
+    <label v-if="label" for="about" class="block text-sm font-medium leading-6 mb-2 text-gray-900">
+      {{ label }}
+    </label>
+
+    <textarea
+      :id="id"
+      :name="label"
+      rows="3"
+      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    />
+
+    <p v-if="description" class="mt-3 text-sm leading-6 text-gray-600">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+
+  id: {
+    type: String,
+    required: true,
+  },
+
+  label: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+
+  placeholder: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+
+  description: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+});
+</script>
