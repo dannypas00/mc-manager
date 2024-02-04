f29357ef861d546c190b695fc9330815f24f1254
dannypas00 <github@dannypas.nl>
2024-02-04 13:28:09 +0100

    Add warning and explanation to input

--- resources/js/Components/Form/FormInput.vue
+++ resources/js/Components/Form/FormInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="w-full">
     <label
       v-if="label"
       :for="id"
@@ -8,23 +8,43 @@
       {{ label }}
     </label>
 
-    <div class="flex rounded-md shadow-sm ring-1 ring-inset pl-2 ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-    <span v-if="$slots.prefix" class="flex select-none items-center pl-1 text-gray-500 sm:text-sm">
-      <slot name="prefix"/>
-    </span>
+    <div class="flex">
+      <div class="flex grow rounded-md shadow-sm ring-1 ring-inset pl-2 ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+        <span v-if="$slots.prefix" class="flex select-none items-center pl-1 text-gray-500 sm:text-sm">
+          <slot name="prefix"/>
+        </span>
 
-      <input
-        :id="id"
-        :placeholder="placeholder"
-        :name="label"
-        type="text"
-        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+        <input
+          :id="id"
+          :placeholder="placeholder"
+          :name="label"
+          type="text"
+          class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+        >
+
+        <div
+          v-if="explanation"
+          class="inline-block p-2"
+          :title="explanation"
+        >
+          <FontAwesomeIcon icon="circle-question"/>
+        </div>
+      </div>
+
+      <div
+        v-if="warning"
+        class="inline-block p-2 text-yellow-500"
+        :title="warning"
       >
+        <FontAwesomeIcon icon="triangle-exclamation"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 defineProps({
   modelValue: {
     type: String,
@@ -43,6 +63,18 @@ defineProps({
   },
 
   placeholder: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+
+  explanation: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+
+  warning: {
     type: String,
     required: false,
     default: undefined,
