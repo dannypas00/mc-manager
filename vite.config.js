d5ca7d11f898a721a524f883ba67b99d78082dc0
Danny Pas <github@dannypas.nl>
2024-08-14 12:18:08 +0200

    Infrastructure copied over

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  optimizeDeps: ['elodo'],
   plugins: [
     laravel({
       input: ['resources/js/app.ts', 'resources/css/app.css'],
