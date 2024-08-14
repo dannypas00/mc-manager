bf87e5ef84e51d0006932c5c180e33699a6ead0c
Danny Pas <github@dannypas.nl>
2024-08-14 21:00:43 +0200

    Add frontend for most of the custom minecraft stuff

---
+++ resources/js/Components/Form/ComboboxInput.vue
@@ -0,0 +1,98 @@
+<template>
+  <Combobox as="div" v-model="selected" @update:modelValue="query = ''">
+    <ComboboxLabel class="block text-sm font-medium leading-6 text-gray-900" v-if="label">
+      {{ label }}
+      <span v-if="required" class="text-red-400">*</span>
+    </ComboboxLabel>
+    <div class="relative mt-2">
+      <ComboboxInput
+        class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        @change="query = $event.target.value"
+        @blur="query = ''"
+      />
+      <ComboboxButton class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+        <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+      </ComboboxButton>
+
+      <ComboboxOptions
+        v-if="filteredOptions.length > 0"
+        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+      >
+        <ComboboxOption
+          v-for="option in filteredOptions"
+          :key="option"
+          :value="option"
+          as="template"
+          v-slot="{ active, selected }"
+        >
+          <li :class="['relative cursor-default select-none py-2 pl-8 pr-4', active ? 'bg-indigo-600 text-white' : 'text-gray-900']">
+            <span :class="['block truncate', selected && 'font-semibold']">
+              {{ option }}
+            </span>
+
+            <span
+              v-if="selected"
+              :class="['absolute inset-y-0 left-0 flex items-center pl-1.5', active ? 'text-white' : 'text-indigo-600']"
+            >
+              <CheckIcon class="h-5 w-5" aria-hidden="true" />
+            </span>
+          </li>
+        </ComboboxOption>
+      </ComboboxOptions>
+    </div>
+  </Combobox>
+</template>
+
+<script setup lang="ts">
+import { computed, PropType, ref } from 'vue';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid';
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxLabel,
+  ComboboxOption,
+  ComboboxOptions,
+} from '@headlessui/vue';
+
+const emit = defineEmits(['update:model-value']);
+
+const props = defineProps({
+  options: {
+    type: Array as PropType<string[]>,
+    required: true,
+  },
+
+  label: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+
+  required: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+const modelValue = defineModel({ type: String, required: true });
+const selected = computed({
+  get () {
+    return modelValue.value;
+  },
+  set (value) {
+    emit('update:model-value', value);
+  },
+});
+
+const selectedPerson = ref(null);
+const query = ref('');
+const filteredOptions = computed(() =>
+  query.value === ''
+    ? props.options
+    : props.options.filter((option: string) => {
+      return option.includes(query.value.toLowerCase());
+    }),
+);
+</script>
--- resources/js/Pages/Servers/Form/ServerMinecraftSegment.vue
+++ resources/js/Pages/Servers/Form/ServerMinecraftSegment.vue
@@ -1,18 +1,97 @@
 <template>
+  <!--
+    Version
+    Custom jar upload
+    server.properties customization
+    Restart schedule (For later)
+    Backup schedule (For later)
+   -->
+  <ToggleButton
+    v-model="serverStore.customizeInstallProcess"
+    :label="$t('pages.servers.form.customize_install.label')"
+  />
+  <div
+    v-if="serverStore.customizeInstallProcess"
+    class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8"
+  >
+    <div class="grid grid-cols-1 gap-y-2">
+      <span v-t="'pages.servers.form.upload_custom_jar_explanation'" />
+      <NeutralButton
+        :text="
+          customJar?.name ?? $t('pages.servers.form.upload_custom_jar_button')
+        "
+        @click="uploadDialog.open"
+      />
+    </div>
+    <FormInput
+      id="custom-docker-image"
+      v-model="serverStore.dockerImage"
+      :label="$t('pages.servers.form.custom_docker_image.label')"
+      :explanation="$t('pages.servers.form.custom_docker_image.explanation')"
+    />
+    <!-- TODO: Custom start script -->
+  </div>
+  <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8" v-else>
+    <ComboboxInput
+      :options="versionOptions"
+      v-model="serverStore.model.version"
+      required
+      :label="$t('pages.servers.form.version.label')"
+    />
+  </div>
+
+  <div class="h-[40vh] w-full">
+    <span v-t="'pages.servers.form.server_properties.label'" />
+    <VueMonacoEditor
+      theme="vs-dark"
+      :options="monacoOptions"
+      language="TOML"
+      v-model:value="serverStore.serverProperties"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ToggleButton from '../../../Components/Form/ToggleButton.vue';
+import ComboboxInput from '../../../Components/Form/ComboboxInput.vue';
+import { useServerEditStore } from '../../../Stores/Servers/ServerEditStore';
+import NeutralButton from '../../../Components/Buttons/NeutralButton.vue';
+import { useFileDialog } from '@vueuse/core';
+import VueMonacoEditor from '@guolao/vue-monaco-editor';
+import FormInput from '../../../Components/Form/FormInput.vue';
 
 export default defineComponent({
-  components: {},
-
-  data() {
-    return {};
+  components: {
+    FormInput,
+    VueMonacoEditor,
+    NeutralButton,
+    ComboboxInput,
+    ToggleButton,
   },
 
-  methods: {},
+  data() {
+    return {
+      serverStore: useServerEditStore(),
+      versionOptions: ['1.21', '1.20', '1.19', '1.18'],
+      uploadDialog: useFileDialog({
+        accept: 'application/java-archive',
+        directory: false,
+        multiple: false,
+      }),
+      customJar: undefined as undefined | File,
+      monacoOptions: {
+        automaticLayout: true,
+        formatOnType: true,
+        formatOnPaste: true,
+      },
+    };
+  },
 
-  computed: {},
+  watch: {
+    'uploadDialog.files'(files: FileList) {
+      this.customJar = files[0];
+    },
+  },
 });
 </script>
