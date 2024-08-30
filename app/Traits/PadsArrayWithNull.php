cd3060deba6ce5776136aaa89ab47c951dd53843
Danny Pas <github@dannypas.nl>
2024-08-30 21:05:43 +0200

    Fix tests for PadsArrayWithNull trait

--- app/Traits/PadsArrayWithNull.php
+++ app/Traits/PadsArrayWithNull.php
@@ -11,7 +11,14 @@ trait PadsArrayWithNull
         return $assocaitiveValues + array_fill_keys($keys, null);
     }
 
-    private function exceptValues(array $input, array $exceptValues): array {
-        return array_flip(Arr::except($input, $exceptValues));
+    private function exceptKeys(array $input, array $exceptValues): array
+    {
+        return array_keys(Arr::except(array_flip($input), $exceptValues));
+    }
+
+    private function getOnlyPaddedFillable(array $fillable, array $data, array $hidden = []): array
+    {
+        $keys = $this->exceptKeys($fillable, $hidden);
+        return $this->padArrayWithNull($keys, Arr::only($data, $keys));
     }
 }
--- tests/Unit/PadsArrayWithNullTest.php
+++ tests/Unit/PadsArrayWithNullTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Unit;
 
 use App\Traits\PadsArrayWithNull;
-
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertEquals;
@@ -21,5 +20,32 @@ class PadsArrayWithNullTest extends TestCase
         ];
         $padded = $this->padArrayWithNull($keys, $values);
         assertEquals(['a' => 'testa', 'b' => null, 'c' => 'testc'], $padded);
+    }
+
+    public function test_that_it_excludes_values(): void
+    {
+        $associative = ['a', 'b', 'c', 'd'];
+        $excludeValues = ['a', 'c'];
+        $expect = ['b', 'd'];
+        $actual = $this->exceptValues($associative, $excludeValues);
+        assertEquals($expect, $actual);
+    }
+
+    public function test_it_excludes_non_fillable_values(): void
+    {
+        $keys = ['a', 'b', 'c'];
+        $values = [
+            'a' => 'testa',
+            'c' => 'testc',
+            'd' => 'testd',
+        ];
+
+        $padded = $this->getOnlyPaddedFillable($keys, $values, ['c']);
+
+        self::assertArrayHasKey('a', $padded);
+        self::assertArrayHasKey('b', $padded);
+        self::assertNull($padded['b']);
+        self::assertArrayNotHasKey('c', $padded);
+        self::assertArrayNotHasKey('d', $padded);
     }
 }
