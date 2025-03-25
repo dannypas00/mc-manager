8b34862a4214c22a8ea87c69287f4cf7d1835201
dannypas00 <github@dannypas.nl>
2025-03-25 07:20:03 +0100

    Fix project setup to align with what shadcn-vue wants

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   plugins: [
     vueDevTools({
       launchEditor: 'phpstorm',
-      appendTo: 'resources/js/app.ts'
+      appendTo: 'resources/js/app.ts',
     }),
     laravel({
       input: ['resources/js/app.ts', 'resources/css/app.css'],
@@ -22,4 +23,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'resources/js'),
+    },
+  },
 });
