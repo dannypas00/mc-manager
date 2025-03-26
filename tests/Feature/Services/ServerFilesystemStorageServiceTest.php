1bb8da35df35b28d5b1e9ea05eabefc90766d5f6
dannypas00 <github@dannypas.nl>
2025-03-26 15:10:13 +0100

    Start list contents tests

--- tests/Feature/Services/ServerFilesystemStorageServiceTest.php
+++ tests/Feature/Services/ServerFilesystemStorageServiceTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature\Services;
 
 use App\Models\Server;
 use App\Services\ServerFilesystemStorageService;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Mockery\MockInterface;
 use Storage;
 
@@ -35,4 +36,15 @@ it('can delete', function (): void {
         ->and($this->service->delete($this->server, 'test-dir'))->toBeTrue()
         ->and($this->filesystem->exists('test.txt'))->toBeFalse()
         ->and($this->filesystem->exists('test-dir'))->toBeFalse();
+});
+
+it('can listContents', function (): void {
+
+})->with([
+    'file'      => ['path' => 'test.txt'],
+    'directory' => ['path' => 'test-dir'],
+]);
+
+it('throws exception when file not found when listing', function (): void {
+    expect(fn () => $this->service->listContents($this->server, 'not-found.txt'))->toThrow(FileNotFoundException::class, 'path_not_found');
 });
